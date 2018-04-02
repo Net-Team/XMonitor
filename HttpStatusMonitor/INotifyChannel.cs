7b359ee069a822f6e449f42982b5ce8a743a160d
intotf@163.com <intotf@163.com>
2018-04-02 14:48:59 +0800

    param

--- HttpStatusMonitor/INotifyChannel.cs
+++ HttpStatusMonitor/INotifyChannel.cs
@@ -14,6 +14,7 @@ namespace HttpStatusMonitor
         /// <summary>
         /// 异常通知
         /// </summary>
+        /// <param name="Title">标题</param>
         /// <param name="ex">异常</param>
         /// <returns></returns>
         Task NotifyAsync(string Title, Exception ex);
