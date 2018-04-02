d48bafd20eb12b5baaf7d5f479ae830754eb0831
intotf <intotf@163.com>
2018-04-02 22:13:26 +0800

    增加 Service 监控

---
+++ ServiceStatusMonitor/ServiceOptions.cs
@@ -0,0 +1,61 @@
+﻿using MonitorServices;
+using NotifyChannels;
+using System;
+using System.Collections.Generic;
+
+namespace ServiceStatusMonitor
+{
+    /// <summary>
+    /// 表示服务状态码监控的配置项
+    /// </summary>
+    public class ServiceOptions
+    {
+        /// <summary>
+        /// 获取或设置检测的时间间隔
+        /// 默认1分钟
+        /// </summary>
+        public TimeSpan Interval { get; set; } = TimeSpan.FromMinutes(1d);
+
+        /// <summary>
+        /// 获取通知通道列表
+        /// </summary>
+        public List<INotifyChannel> NotifyChannels { get; } = new List<INotifyChannel>();
+
+        /// <summary>
+        /// 获取目标服务列表
+        /// </summary>
+        public List<string> ServiceNames { get; set; } = new List<string>();
+
+        /// <summary>
+        /// 获取或设置日志工具
+        /// </summary>
+        public ILogger Logger { get; set; }
+
+
+        /// <summary>
+        /// 使用邮件通知 
+        /// </summary>
+        /// <param name="options">选项</param>
+        public void UseEmailNotifyChannel(Action<EmailChannelOptions> options)
+        {
+            var opt = new EmailChannelOptions();
+            options?.Invoke(opt);
+
+            var channel = new EmailChannel(opt);
+            this.NotifyChannels.Add(channel);
+        }
+
+        /// <summary>
+        /// 使用Http通知 
+        /// </summary>
+        /// <param name="options">选项</param>
+        public void UseHttpNotifyChannel(Action<HttpChannelOptions> options)
+        {
+            var opt = new HttpChannelOptions();
+            options?.Invoke(opt);
+
+            var channel = new HttpChannel(opt);
+            this.NotifyChannels.Add(channel);
+        }
+    }
+}
---
+++ ServiceStatusMonitor/ServiceStatusService.cs
@@ -0,0 +1,75 @@
+﻿using MonitorServices;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceStatusMonitor
+{
+    class ServiceStatusService : IMonitorService
+    {
+        /// <summary>
+        /// 选项
+        /// </summary>
+        private readonly ServiceOptions options;
+        /// <summary>
+        /// 获取或设置是否在运行
+        /// </summary>
+        public bool IsRunning { get; private set; }
+
+        /// <summary>
+        /// 当前服务
+        /// </summary>
+        private ServiceController _service { get; set; }
+
+        /// <summary>
+        /// http状态检测服务
+        /// </summary>
+        /// <param name="options">选项</param>
+        public ServiceStatusService(ServiceOptions options)
+        {
+            this.options = options;
+        }
+
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 获取服务运行状态
+        /// </summary>
+        /// <param name="serviceName"></param>
+        /// <returns></returns>
+        private bool CheckServiceStatus(string serviceName)
+        {
+            this._service = new ServiceController(serviceName);
+            return this._service.Status != ServiceControllerStatus.Stopped;
+        }
+
+        /// <summary>
+        /// 运行检测
+        /// </summary>
+        /// <returns></returns>
+        private async Task RunAsync()
+        {
+            while (this.IsRunning == true)
+            {
+                foreach (var item in this.options.ServiceNames.Distinct())
+                {
+                    this.CheckServiceStatus(item);
+                }
+                await Task.Delay(this.options.Interval);
+            }
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
