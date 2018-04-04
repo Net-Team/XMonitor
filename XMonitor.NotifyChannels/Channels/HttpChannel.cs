5cb325b664baf7debd8061f06f9f1e80d7fbaca2
intotf@163.com <intotf@163.com>
2018-04-04 11:51:43 +0800

    IHttpNotifyClient 独立Public

--- XMonitor.NotifyChannels/Channels/HttpChannel.cs
+++ XMonitor.NotifyChannels/Channels/HttpChannel.cs
@@ -8,31 +8,13 @@ using XMonitor.Core;
 
 namespace XMonitor.NotifyChannels
 {
+
+
     /// <summary>
     /// 表示Http异常通知通道
     /// </summary>
     class HttpChannel : INotifyChannel
     {
-        /// <summary>
-        /// Http 异常通知接口
-        /// </summary>
-        public interface IHttpNotifyClient : IDisposable
-        {
-            /// <summary>
-            /// 发送Http请求
-            /// </summary>
-            /// <param name="url"></param>
-            /// <param name="header">请求头</param>
-            /// <param name="content">内容</param>
-            /// <returns></returns>
-            [HttpPost]
-            ITask<HttpResponseMessage> SendNotifyAsync(
-                [Url] Uri url,
-                [Headers] IEnumerable<KeyValuePair<string, string>> header,
-                [FormContent] IEnumerable<KeyValuePair<string, string>> content);
-        }
-
-
         /// <summary>
         /// 选项
         /// </summary>
@@ -68,5 +50,24 @@ namespace XMonitor.NotifyChannels
             await this.httpNotifyClient
                 .SendNotifyAsync(this.opt.Uri, this.opt.Header, httpContent);
         }
+    }
+
+    /// <summary>
+    /// Http 异常通知接口
+    /// </summary>
+    public interface IHttpNotifyClient : IDisposable
+    {
+        /// <summary>
+        /// 发送Http请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="header">请求头</param>
+        /// <param name="content">内容</param>
+        /// <returns></returns>
+        [HttpPost]
+        ITask<HttpResponseMessage> SendNotifyAsync(
+            [Url] Uri url,
+            [Headers] IEnumerable<KeyValuePair<string, string>> header,
+            [FormContent] IEnumerable<KeyValuePair<string, string>> content);
     }
 }
---
+++ XMonitor.Performance/PerformanceMonitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using XMonitor.Core;
+
+namespace XMonitor.Performance
+{
+    /// <summary>
+    /// 性能监控
+    /// </summary>
+    public class PerformanceMonitor : IMonitor
+    {
+        /// <summary>
+        /// 获取或设置别名
+        /// </summary>
+        public string Alias { get; set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public object Value => throw new NotImplementedException();
+    }
+}
